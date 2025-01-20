b02006e04a86cd52be8cd1e18c217c14cc8ddaa8
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2025-01-20 12:29:45 +0100

    Fixed issue #2 + layout save

--- main.cpp
+++ main.cpp
@@ -54,7 +54,7 @@ bool sendCmd(const std::string& cmd, const std::string& port, std::string& buff)
     return false;
 }
 
-bool sendCommandUsingFile(std::string& cmd, const std::string& port, std::string& buff, std::string targetFile) {
+bool sendCommandUsingFile(std::string& cmd, const std::string& port, std::string& buff, std::string targetFile = "") {
     std::ofstream file("buff.cache");
     if (!file.is_open()) {
         return false;
@@ -65,43 +65,52 @@ bool sendCommandUsingFile(std::string& cmd, const std::string& port, std::string
         int num = -1;
         int index = 0;
         bool firstTime = true;
+        std::string newLine = "";
         for (char& c : cmd) {
             ++num;
             if (num % 5000 == 0 && !firstTime) {
                 ++index;
+                lines.push_back(newLine);
+            }
+
+            if (firstTime) {
+                lines.push_back(newLine);
+                firstTime = false;
             }
 
             lines[index] += c;
-            if (firstTime) { firstTime = false; }
         }
 
         num = 0;
         index = 0;
         for (std::string l : lines) {
             ++num;
-            file << "b" << num << " = '" << l << "'\n"
+            file << "b" << num << " = '" << l << "'\n";
         }
 
-        file << "f = open('" + targetFile + "', 'w'); f.write("
+        file << "f = open('" + targetFile + "', 'w'); f.write(";
 
         std::string delStr = "";
         for (int x = 0; x < num; x++) {
             if (x != 0) {
                 file << " ";
+                delStr += " ";
             }
             file << "b" << (x + 1);
-            delStr += "";
+            delStr += "del b" + std::to_string(x + 1) + ";";
             if (x != (num - 1)) {
                 file << " +";
             }
         }
 
-        file << "); "
+        file << "); f.close(); " << delStr;
+        file.close();
     }
     else {
         file << cmd << "\n";
         file.close();
     }
+
     dllMethods.quietShell(("for /f \"usebackq delims=\" %a in (buff.cache) do @echo %a > COM" + port).c_str(), buff);
     createUsefulBuff(buff);
     if (buff == "") { return true; }
@@ -219,116 +228,118 @@ int main() {
     dllMethods.quietShell("mkdir workspace", cacheBuff);
     cacheBuff = "";
 
-    dllMethods.quietShell("cd workspace && powershell \"Get-ChildItem -Recurse | ForEach-Object { $_.FullName.Substring($pwd.Path.Length + 1) }\"", buff);
-    createUsefulBuff(buff);
+    while (true) {
+        dllMethods.quietShell("cd workspace && powershell \"Get-ChildItem -Recurse | ForEach-Object { $_.FullName.Substring($pwd.Path.Length + 1) }\"", buff);
+        createUsefulBuff(buff);
 
-    std::string lines = buff + "\n";// \n is added, to recieve the last item from the buff
-    buff = "";
+        std::string lines = buff + "\n";// \n is added, to recieve the last item from the buff
+        buff = "";
 
-    std::string tmpLine = "";
-    std::vector<std::string> files;
-    for (char& c : lines) {
-        if (c == '\n') {
-            files.push_back(tmpLine);
-            tmpLine = "";
-            continue;
-        }
-        if (c == '\\') {
-            c = '/';
-        }
-        tmpLine += c;
-    }
-
-    //Check for deleted files or folders
-    deletedFiles delFiles = filesGotDeleted(files, prevFiles);
-    if (delFiles.deleted()) {
-        //Send command to delete files on esp32
-        //std::cout << delFiles.files()[0] << "\n";
-        for (std::string f : delFiles.files()) {
-            theGreatEscape(f);
-            std::string delCmd = "import uos; uos.remove('" + f + "');";
-            //std::cout << delCmd << "\n";
-            if (!sendCommandUsingFile(delCmd, serPort, buff)) {
-                couldNotSendToCOM(serPort, buff);
-                return 3;
-            }
-        }
-        //
-        //std::cout << "Files got deleted...\n";
-    }
-    //
-
-    prevFiles = files;
-
-    for (std::string& filePath : files) {
-        std::string realFilePath = "./workspace/" + filePath;
-        std::string fileContents = "";
-        bool isFolder = false;
-        tmpLine = "";
-        std::ifstream file(realFilePath);
-        if (!file.is_open()) {
-            bool invertedFolderCheck = true;
-            //Check if 'file' is folder
-            for (char& c : filePath) {
-                if (c == '.') {
-                    invertedFolderCheck = false;
-                    break;
-                }
-            }
-            if (invertedFolderCheck) {
-                //std::cout << filePath << " is a folder...\n";
-                isFolder = true;
-            }
-            else {
-                std::cout << "Could not open file " << realFilePath << ". Skipping...\n";
+        std::string tmpLine = "";
+        std::vector<std::string> files;
+        for (char& c : lines) {
+            if (c == '\n') {
+                files.push_back(tmpLine);
+                tmpLine = "";
                 continue;
             }
-            //
-        }
-
-        std::string cmdTargetFile = "";
-        std::string cmd = "";
-
-        if (!isFolder) {//Get content out of file
-            while (std::getline(file, tmpLine)) {
-                fileContents += tmpLine + "\n";
+            if (c == '\\') {
+                c = '/';
             }
+            tmpLine += c;
+        }
 
-            //Escape escaping
-            theGreatEscape(fileContents);
+        //Check for deleted files or folders
+        deletedFiles delFiles = filesGotDeleted(files, prevFiles);
+        if (delFiles.deleted()) {
+            //Send command to delete files on esp32
+            //std::cout << delFiles.files()[0] << "\n";
+            for (std::string f : delFiles.files()) {
+                theGreatEscape(f);
+                std::string delCmd = "import uos; uos.remove('" + f + "');";
+                //std::cout << delCmd << "\n";
+                if (!sendCommandUsingFile(delCmd, serPort, buff)) {
+                    couldNotSendToCOM(serPort, buff);
+                    return 3;
+                }
+            }
             //
-
-            cmdTargetFile = filePath;
-            cmd = fileContents;
-        }
-        else {
-            if (filePath == "") { continue; }
-            cmd = "import uos; uos.mkdir('" + filePath + "')";
-        }
-
-        //std::cout << cmd << "\n";
-
-        //Timeout to give esp32 time to write the file
-        size_t fileSize = strlen(fileContents.c_str());
-        if (fileSize > 3000) {
-            Sleep(3500);
-        }
-        else {
-            Sleep(fileSize / 10);
+            //std::cout << "Files got deleted...\n";
         }
         //
 
-        if (!sendCommandUsingFile(cmd, serPort, buff, cmdTargetFile)) {
-            couldNotSendToCOM(serPort, buff);
-            return 4;
-        }
+        prevFiles = files;
 
-        file.close();
+        for (std::string& filePath : files) {
+            std::string realFilePath = "./workspace/" + filePath;
+            std::string fileContents = "";
+            bool isFolder = false;
+            tmpLine = "";
+            std::ifstream file(realFilePath);
+            if (!file.is_open()) {
+                bool invertedFolderCheck = true;
+                //Check if 'file' is folder
+                for (char& c : filePath) {
+                    if (c == '.') {
+                        invertedFolderCheck = false;
+                        break;
+                    }
+                }
+                if (invertedFolderCheck) {
+                    //std::cout << filePath << " is a folder...\n";
+                    isFolder = true;
+                }
+                else {
+                    std::cout << "Could not open file " << realFilePath << ". Skipping...\n";
+                    continue;
+                }
+                //
+            }
+
+            std::string cmdTargetFile = "";
+            std::string cmd = "";
+
+            if (!isFolder) {//Get content out of file
+                while (std::getline(file, tmpLine)) {
+                    fileContents += tmpLine + "\n";
+                }
+
+                //Escape escaping
+                theGreatEscape(fileContents);
+                //
+
+                cmdTargetFile = filePath;
+                cmd = fileContents;
+            }
+            else {
+                if (filePath == "") { continue; }
+                cmd = "import uos; uos.mkdir('" + filePath + "')";
+            }
+
+            //std::cout << cmd << "\n";
+
+            //Timeout to give esp32 time to write the file
+            size_t fileSize = strlen(fileContents.c_str());
+            if (fileSize > 3000) {
+                Sleep(3500);
+            }
+            else {
+                Sleep(fileSize / 10);
+            }
+            //
+
+            if (!sendCommandUsingFile(cmd, serPort, buff, cmdTargetFile)) {
+                couldNotSendToCOM(serPort, buff);
+                return 4;
+            }
+
+            file.close();
+        }
+        dllMethods.quietShell("del buff.cache", buff);
+        buff = "";
+        cacheBuff = "";
+        std::time_t now = std::time(nullptr);
+        std::tm* localTime = std::localtime(&now);
+        std::cout << "Last updated: " << (localTime->tm_hour < 10 ? "0" : "") << localTime->tm_hour << ":" << (localTime->tm_min < 10 ? "0" : "") << localTime->tm_min << ":" << (localTime->tm_sec < 10 ? "0" : "") << localTime->tm_sec << "\n";
     }
-    dllMethods.quietShell("del buff.cache", buff);
-    buff = "";
-    cacheBuff = "";
-    std::time_t now = std::time(nullptr);
-    std::tm* localTime = std::localtime(&now);
-    std::cout << "Last updated: " << (localTime->tm_hour < 10 ? "0" : "") << localTime->tm_hour << ":" << (localTime->tm_min < 10 ? "0" : "") << localTime->tm_min << ":" << (localTime->tm_sec < 10 ? "0" : "") << localTime->tm_sec << "\n";
 }
